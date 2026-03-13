09b7f2413f8b1ae4a8e9dbf966e69a601484fe71
wangjt <wangjt@dragoninfo.com.cn>
2026-03-13 12:19:58 +0800

    feat: 更新面试题库静态资源文件
    添加新的面试题目录和文件，并删除旧版本的文件
    更新各面试题模块的元数据，包括最后更新时间
    优化静态资源文件的组织和命名

--- interview-bank/assets/frontend_vue_vue2_02-core-principles.md.Bj33LRWB.lean.js
+++
@@ -1,4 +0,0 @@
-import{_ as e,C as i,o as l,c as n,ag as o,E as s}from"./chunks/framework.ClpDZR9J.js";const V=JSON.parse('{"title":"Vue 核心原理","description":"","frontmatter":{},"headers":[],"relativePath":"frontend/vue/vue2/02-core-principles.md","filePath":"frontend/vue/vue2/02-core-principles.md"}'),a={name:"frontend/vue/vue2/02-core-principles.md"};function g(u,t,d,c,p,h){const r=i("MermaidChart");return l(),n("div",null,[t[0]||(t[0]=o("",57)),s(r,{code:`graph TD
-    A["核心层：基础视图渲染<br>（Vue 核心库，仅做数据绑定/指令）"] --> B["进阶层：组件化路由(Vue Router)"]
-    B --> C["高级层：状态管理(Vuex/Pinia)"]
-    C --> D["工程化层：构建工具(Vite/Webpack)/TS支持"]`}),t[1]||(t[1]=o("",19))])}const b=e(a,[["render",g]]);export{V as __pageData,b as default};
